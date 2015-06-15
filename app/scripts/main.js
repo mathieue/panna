ccc4a43cef8271afbb73e1a67fc2549b8dfcdbbc
Mathieu ELIE <mathieuelie@gmail.com>
2015-06-15 07:42:45 +0100

    d3 circles hello world.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+var width = 600,
+	height = 600;
+
+var r = 6, padding = 4;
+
+// i want a matrix
+var data = [];
+for (var i = 0; i < 100; i++) {
+	for (var j = 0; j < 100; j++) {
+		data.push({x: i, y: j});	
+	};
+	
+};
+
+var svg = d3.select("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var circle = svg.selectAll("circle")
+    .data(data);
+
+var circleEnter = circle.enter().append("circle");
+circleEnter.attr("cx", function(d, i) { return d.x * r + r / 2; });
+circleEnter.attr("cy", function(d, i) { return d.y * r + r / 2; });
+circleEnter.attr("r", r - padding);
