c100e3ec603a006611eab7857e954171d064c63f
Mathieu ELIE <mathieuelie@gmail.com>
2015-06-15 09:19:53 +0100

    node server as simple http proxy. calling openbeelab server.

---
+++ node-server/server.js
@@ -0,0 +1,48 @@
+var http = require('http')
+  , server = http.createServer();
+
+server.on('request', function (serverReq,
+    serverResp) {
+
+  var host = 'dev.openbeelab.org'
+  	, port = 5984
+    , opts
+    , clientRequest
+    , handle;
+
+  handle = setTimeout(function () {
+    serverResp.writeHead(500, {});
+    serverResp.end('dammit');
+  }, 5000);
+
+
+  serverReq.headers.host = host;
+  opts = {
+    host: host
+  , port: port
+  , path: serverReq.url
+  , headers: serverReq.headers
+  };
+  clientReq = http.request(opts);
+
+  clientReq.on('response', function (clientResp) {
+    serverResp.writeHead(clientResp.statusCode,
+      clientResp.headers);
+    clientResp.on('data', function (data) {
+      serverResp.write(data);
+    });
+    clientResp.on('end', function () {
+      clearTimeout(handle);
+      serverResp.end();
+    });
+  });
+  console.log('requesting ' + serverReq.url);
+
+  clientReq.end();
+});
+
+server.listen(4000, 'localhost');
+
+process.on('uncaughtException', function (err) {
+  console.log(err.message);
+});
